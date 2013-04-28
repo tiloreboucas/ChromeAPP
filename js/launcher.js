f62c22d32b7587d44f0bfe10a15f49c89a49df39
Witalo <tilowr@gmail.com>
2013-04-27 22:48:07 -0300

    Atualização do Launcher

--- js/launcher.js
+++ js/launcher.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('home.html', {
+  chrome.app.window.create('index.html', {
   	'id' : 'main',
     'bounds': {
       'width': 480,
